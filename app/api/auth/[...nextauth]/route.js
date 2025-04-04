2b82ae64267f2f749bde83b6204f85dc6c1a46d7
Desktoo <akshatkri1463@gmail.com>
2025-04-04 16:11:07 +0530

    Fix NextAuth route for Vercel deployment

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google"
-import { connectToDB } from "@utils/database";
-import User from '@models/user'
+import { connectToDB } from "../../../../utils/database.js";
+import User from '../../../../models/user.js'
 
 const handler = NextAuth({
     providers: [
